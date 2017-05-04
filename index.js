d4d4bd14a523da8929f40177846285524af4bf38
radhakrishnan S <radhakrishnan.s@myntra.com>
2017-05-04 16:47:27 +0530

    Enable file upload ability

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 console.log('hello from Node.js')
 const express = require('express')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const mutler = require('mutler')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -12,7 +14,23 @@ app.listen(port, (error) => {
   if (error)
     return console.log('something bad happened',error)
 })
-app.post('/profile', upload.single('image'), function (req, res, next) {
+
+app.post('/shotupload', upload.single('image'), function (req, res, next) {
+  console.log('request image',req.file)
+  if (req.file) {
+    const responseJSON = {
+      data: {
+        id: "30970182",
+      //  url: {id:52635738,relativePath:"assets/images/2017/5/4/11493872523180-43120-dxv5bc.jpg",size:803955,domain:"http://assets.myntassets.com/",securedDomain:"https://assets.myntassets.com/","resolutionFormula":"h_($height),q_($qualityPercentage),w_($width)/v1/assets/images/2017/5/4/11493872523180-43120-dxv5bc.jpg","path":"http://assets.myntassets.com/assets/images/2017/5/4/11493872523180-43120-dxv5bc.jpg","storedUploaderType":CL,"servingUploaderType:"CL",expireAfter:1495600523200}
+      },
+      meta: {
+        code: 200
+      }
+    }
+    res.send(JSON.stringify(responseJSON))
+  }else{
+    res.send('Error ')
+  }
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
 })
@@ -20,8 +38,8 @@ app.get('/',(request, response) => {
   console.log(`server is listening in port ${port}`)
   response.send('Hello from express')
 })
-app.post('/shotupload',(request, response) => {
-  console.log('body',request.body)
+app.post('/temp',(request, response) => {
+  console.log('request',request.file)
   if (request.image) {
     console.log(`image data is ${request.image}`)
   }
